71e0e92851bfdb6704906e45edfd8fe8e9406fb2
neuroproxy <cgcj979@gmail.com>
2023-05-05 21:58:52 -0400

    Modificacion de la herramienta xss-inject

--- XSS-Inject.py
+++ XSS-Inject.py
@@ -30,6 +30,27 @@ def get_form_details(form):
     details["inputs"] = inputs
     return details
 
+# Funcion a la que pasamos los parametros como el HTML, la URL y el valor que contendra los campos potenciales
+def submit_for(form_details, url, value):
+    target_url = urljoin(url, form_details["actions"])
+    inputs = form_details["inputs"]
+    data = {}
+    # Bucle que recorre el arreglo inputs y verifica que si los campos inyectables son de tipo text o search, seran iguales a value
+    for input in inputs:
+        if input["type"] == "text" or input["type"] == "search":
+            input["value"] = value
+        input_name = input.get("name")
+        input_value = input.get("value")
+        if input_name and input_value:
+            data[input_name] = input_value
+    print(f"[+] Enviando payload hacia: {target_url}")
+    print(f"[+] Data: {data}")
+    if form_details["method"] == "post":
+        return requests.post(target_url, data=data)
+    else:
+        return requests.get(target_url, params=data)
+    
+
 # Funcion que sirve para inyectar el payload, recibe como parametro la URL y aplica el proceso de la funcion get_all_forms
 # Al presentarse campos de entrada en los metodos recopilados inyecta el payload js y establecemos una var bool para cambiar su valor si el sitio es vulnerable
 def scan_xss(url):
